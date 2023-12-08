d48e0d120bdf83e98983c4498c6b4eac3231abd5
Sarkies <Ahmed.Sarkes@gmail.com>
2023-12-09 01:00:05 +0200

    Update ProductCard component using redux

--- src/components/UI/ProductCard/ProductCard.jsx
+++ src/components/UI/ProductCard/ProductCard.jsx
@@ -2,10 +2,26 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
+import { useDispatch } from "react-redux";
+
+import { addToCart } from "../../../store/slices/cartSlice";
+
 import "./product-card.css";
 
 const ProductCard = ({ product }) => {
-    const {id, title, price, image01} = product;
+  const { id, title, price, image01 } = product;
+  const dispatch = useDispatch();
+
+  const addToCartHandler = () => {
+    dispatch(
+      addToCart({
+        ...product,
+        quantity: 1,
+        totalPrice: price,
+      })
+    );
+  };
+
   return (
     <div className="product__item">
       <div className="product__img">
@@ -17,7 +33,9 @@ const ProductCard = ({ product }) => {
         </h5>
         <div className="d-flex align-items-center justify-content-between">
           <span className="product__price">${price}</span>
-          <button className="addToCart__btn">Add to Cart</button>
+          <button className="addToCart__btn" onClick={addToCartHandler}>
+            Add to Cart
+          </button>
         </div>
       </div>
     </div>
