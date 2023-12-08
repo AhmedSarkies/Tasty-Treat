ee3790b02e8e7d8b41956c8d6e307deaf5600dd3
Sarkies <Ahmed.Sarkes@gmail.com>
2023-12-09 00:57:39 +0200

    Update Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,24 @@
-import React from "react";
-
-import { Header, Footer } from "../";
+import React, { Fragment, useEffect } from "react";
 
 import Routers from "../../routes/Routers";
-import { Fragment } from "react";
+
+import { useDispatch } from "react-redux";
+
+import { loadCartItems } from "../../store/slices/cartSlice";
+
+import { Header, Footer, Cart } from "../";
 
 const Layout = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadCartItems());
+  }, [dispatch]);
+
   return (
     <Fragment>
       <Header />
+      <Cart />
       <Fragment>
         <Routers />
       </Fragment>
