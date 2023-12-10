62a1995511b812f0639361b987157a118a588f09
Sarkies <Ahmed.Sarkes@gmail.com>
2023-12-10 03:46:19 +0200

    update in CartItem Component

--- src/components/UI/Cart/CartItem/CartItem.jsx
+++ src/components/UI/Cart/CartItem/CartItem.jsx
@@ -12,13 +12,13 @@ import {
 
 const CartItem = ({ cartItem }) => {
   const dispatch = useDispatch();
+  const { id, title, image01, price, quantity } = cartItem;
 
   const addToCartHandler = () => {
     dispatch(
       addToCart({
         ...cartItem,
         quantity: 1,
-        totalPrice: cartItem.price,
       })
     );
   };
@@ -34,25 +34,21 @@ const CartItem = ({ cartItem }) => {
   return (
     <ListGroupItem className="cart__item border-0">
       <div className="cart__item-info d-flex gap-2">
-        <img
-          src={cartItem.image01}
-          alt={cartItem.title}
-          className="product-img"
-        />
+        <img src={image01} alt={title} className="product-img" />
         <div className="cart__product-info w-100 d-flex align-items-center justify-content-between gap-4">
           <div>
-            <h6 className="cart__product-title">{cartItem.title}</h6>
+            <h6 className="cart__product-title">{title}</h6>
             <p className="cart__product-price d-flex align-items-center gap-5">
-              {cartItem.quantity}x <span>${cartItem.price}</span>
+              {quantity}x <span>${price * quantity}</span>
             </p>
             <div className="increase__decrease-btn d-flex align-items-center justify-content-between">
               <span className="increase__btn" onClick={addToCartHandler}>
                 <i className="ri-add-line"></i>
               </span>
-              <span className="quantity">{cartItem.quantity}</span>
+              <span className="quantity">{quantity}</span>
               <span
                 className="decrease__btn"
-                onClick={() => deleteFromCartHandler(cartItem.id)}
+                onClick={() => deleteFromCartHandler(id)}
               >
                 <i className="ri-subtract-line"></i>
               </span>
@@ -60,8 +56,8 @@ const CartItem = ({ cartItem }) => {
           </div>
           <span className="delete__btn">
             <i
-              className="ri-delete-bin-line"
-              onClick={() => removeItemFromCartHandler(cartItem.id)}
+              className="ri-close-line"
+              onClick={() => removeItemFromCartHandler(id)}
             ></i>
           </span>
         </div>
