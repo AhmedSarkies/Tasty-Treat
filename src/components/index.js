2527bd9dd1f49b52ffeda57f53747513f4d280fa
Sarkies <Ahmed.Sarkes@gmail.com>
2023-12-12 08:43:40 +0200

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -7,3 +7,5 @@ export { default as ProductCard } from "./UI/ProductCard/ProductCard";
 export { default as TestimonialSlider } from "./UI/Slider/TestimonialSlider";
 export { default as Cart } from "./UI/Cart/Cart";
 export { default as CartItem } from "./UI/Cart/CartItem/CartItem";
+export { default as CommonSection } from "./UI/CommonSection/CommonSection";
+export { default as Pagination } from "./UI/Pagination/Pagination";
