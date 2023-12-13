2498eb5bc753fc858bc37b095ccf732e187cbd9a
Sarkies <Ahmed.Sarkes@gmail.com>
2023-12-13 22:25:44 +0200

    Create Cart Page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+
+import { Col, Container, Row } from "reactstrap";
+
+import { removeItemFromCart } from "../store/slices/cartSlice";
+
+import { CommonSection, Helmet } from "../components";
+
+import "../styles/cart.css";
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart.cart);
 
-export default Cart
+  const deleteItem = (id) => {
+    dispatch(removeItemFromCart(id));
+  };
+
+  return (
+    <Helmet title="Cart">
+      <CommonSection title="Your Cart" />
+      <section>
+        <Container>
+          <Row>
+            {cart.length === 0 && (
+              <Col lg="12" className="cart__empty">
+                <p className="text-center">Your cart is empty</p>
+              </Col>
+            )}
+            {cart.length !== 0 && (
+              <Col lg="12" className="cart__table">
+                <table className="table w-100">
+                  <thead>
+                    <tr>
+                      <th>Image</th>
+                      <th>Product Title</th>
+                      <th>Price</th>
+                      <th>Quantity</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {cart.map((item) => (
+                      <tr key={item.id}>
+                        <td>
+                          <img
+                            src={item.image01}
+                            alt={item.title}
+                            style={{
+                              width: "50px",
+                              height: "50px",
+                              objectFit: "cover",
+                            }}
+                          />
+                        </td>
+                        <td>{item.title}</td>
+                        <td>${item.price}</td>
+                        <td>{item.quantity}</td>
+                        <td>
+                          <button
+                            className="btn btn-danger"
+                            onClick={() => deleteItem(item.id)}
+                          >
+                            Remove
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </Col>
+            )}
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default Cart;
