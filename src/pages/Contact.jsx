c6ca19a1473f6d6817cb45d92d4110f472fd5aeb
Sarkies <Ahmed.Sarkes@gmail.com>
2023-12-17 05:43:24 +0200

    Create Contact Page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import { Col, Container, Row } from "reactstrap";
+import { CommonSection, Helmet } from "../components";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
 
-export default Contact
+  const resetForm = () => {
+    setName("");
+    setEmail("");
+    setSubject("");
+    setMessage("");
+  };
+
+  const sendEmailHandler = (e) => {
+    e.preventDefault();
+    resetForm();
+  };
+
+  return (
+    <Helmet title="Contact Us">
+      <CommonSection title="Contact Us" />
+      <section>
+        <Container>
+          <Row>
+            <Col xxl="8" xl="10" md="12" className="m-auto text-center">
+              <form
+                className="form login__form checkout__form"
+                onSubmit={sendEmailHandler}
+              >
+                <div className="form__group mb-3">
+                  <input
+                    type="text"
+                    placeholder="Name *"
+                    required
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div className="form__group">
+                  <input
+                    type="email"
+                    placeholder="Email *"
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className="form__group">
+                  <input
+                    type="text"
+                    placeholder="Subject *"
+                    required
+                    value={subject}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className="form__group">
+                  <textarea
+                    name="message"
+                    id="message"
+                    cols="30"
+                    rows="3"
+                    placeholder="Message *"
+                    required
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                  ></textarea>
+                </div>
+                <button
+                  type="submit"
+                  value="Send"
+                  className="addToCart__btn payment__btn"
+                >
+                  Contact Us
+                </button>
+              </form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default Contact;
